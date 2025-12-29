8e688365e044d22e9361b4e5d12f714ecfa2d266
Márcio Miliano Gange Daniel <marciodaniel35@gmail.com>
2025-12-29 20:17:40 +0100

    Estrutura de repetição
    for(var cont = 0; cont < num.length; cont++){
        console.log(num[cont])
    }
    
    for(let pos in num){
        console.log(num[pos])
    }

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [24, 25, 29, 1, 5, 7 , 8]
+//console.log(`O vetor tem ${num.length} posições`)
+//console.log(`O primeiro valor do vetor é ${num[0]}`)
+//console.log(`O último elemento do vetor é valor do veto é ${num[num.length - 1]}`)
+/*
+for(var cont = 0; cont < num.length; cont++){
+    console.log(num[cont])
+
+}*/
+
+for(let pos in num){
+    console.log(num[pos])
+}
